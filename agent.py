63e9cbdf05e2450da0fca5892109f53f02a234e7
dnst-agent <minhhoatrinh96@gmail.com>
2026-06-16 15:55:30 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -80,4 +80,4 @@ async def api_ai(req: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
